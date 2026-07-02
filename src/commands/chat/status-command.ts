ce2b2497dd247030c4ca70d8c8c7d2c7e17c91fe
Samy Rahmani <samy.osmium@gmail.com>
2026-07-02 18:10:30 -0400

    feat: added peter

---
+++ src/commands/chat/status-command.ts
@@ -0,0 +1,32 @@
+import { ChatInputCommandInteraction, PermissionsString } from 'discord.js';
+import { RateLimiter } from 'discord.js-rate-limiter';
+
+import { EventData } from '../../models/internal-models.js';
+import { InteractionUtils } from '../../utils/index.js';
+import { Command, CommandDeferType } from '../index.js';
+
+interface Item {
+    is_alive: boolean;
+    service: string;
+    uptime: string;
+}
+
+export class StatusCommand implements Command {
+    public names = ["status"];
+    public cooldown = new RateLimiter(1, 1000);
+    public deferType = CommandDeferType.HIDDEN;
+    public requireClientPerms: PermissionsString[] = [];
+
+    public async execute(intr: ChatInputCommandInteraction, data: EventData): Promise<void> {
+        const message = await intr.channel.send("Récupération des données...");
+        const content = await (await fetch("https://culture.lrima.ca/uptime")).json() as Item[] | null;
+        if (!content) {
+            message.edit("Impossible de récupérer les données. Il se peut que le server soit down.")
+            return;
+        }
+        const item_answers = content.map(item => `- ${item.service}: ${item.is_alive ? `✅ En marche (Uptime: *${item.uptime}*)` : "❌ Pas en vie"}`);
+        const header = "**États des services**: \n";
+        message.edit(header + item_answers.join("\n"));
+        InteractionUtils.send(intr, "Données envoyées");
+    }
+}
---
+++ src/commands/metadata.ts
@@ -0,0 +1,48 @@
+import {
+    ApplicationCommandType,
+    PermissionFlagsBits,
+    PermissionsBitField,
+    RESTPostAPIChatInputApplicationCommandsJSONBody,
+    RESTPostAPIContextMenuApplicationCommandsJSONBody,
+} from 'discord.js';
+
+import { Args } from './index.js';
+import { Language } from '../models/enum-helpers/index.js';
+import { Lang } from '../services/index.js';
+
+export const ChatCommandMetadata: {
+    [command: string]: RESTPostAPIChatInputApplicationCommandsJSONBody;
+} = {
+    STATUS: {
+        type: ApplicationCommandType.ChatInput,
+        name: "status",
+        description: Lang.getRef('commandDescs.test', Language.Default),
+        description_localizations: Lang.getRefLocalizationMap('commandDescs.test'),
+        dm_permission: true,
+        default_member_permissions: undefined,
+    },
+};
+
+export const MessageCommandMetadata: {
+    [command: string]: RESTPostAPIContextMenuApplicationCommandsJSONBody;
+} = {
+    VIEW_DATE_SENT: {
+        type: ApplicationCommandType.Message,
+        name: Lang.getRef('messageCommands.viewDateSent', Language.Default),
+        name_localizations: Lang.getRefLocalizationMap('messageCommands.viewDateSent'),
+        default_member_permissions: undefined,
+        dm_permission: true,
+    },
+};
+
+export const UserCommandMetadata: {
+    [command: string]: RESTPostAPIContextMenuApplicationCommandsJSONBody;
+} = {
+    VIEW_DATE_JOINED: {
+        type: ApplicationCommandType.User,
+        name: Lang.getRef('userCommands.viewDateJoined', Language.Default),
+        name_localizations: Lang.getRefLocalizationMap('userCommands.viewDateJoined'),
+        default_member_permissions: undefined,
+        dm_permission: true,
+    },
+};
